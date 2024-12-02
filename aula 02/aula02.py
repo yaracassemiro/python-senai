a8eeeba80c7ffb84cc66bcfbce66f16b05a0c1ae
yaracassemiro <yaracassemiroliveiraa@gmail.com>
2024-12-02 10:00:15 -0300

    aprendi o que é uma variavel

---
+++ aula 02/aula02.py
@@ -0,0 +1,8 @@
+print(´olá)
+      
+      nome_usuario=´Yara´#tipo->str=string
+      idade_usuario=16#tipo_>int=integer
+      altura_usuario=1.58#tipo_>float
+
+      maior_de_idade=False#tipo->bool=boolean
+      print(`seja bem-vindo`,nome_usuario,`sua idade é`,idade_usuario,`sua altura é,´ altura_usuario,`vocÊ é maior de idade?´,maior_de_idade)
